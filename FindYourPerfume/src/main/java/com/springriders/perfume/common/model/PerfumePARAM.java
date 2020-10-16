92c0e0ec25af61ea609092f5f02bebb239fcc19b
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-10-16 11:26:24 +0900

    2020-10-16 (PerfumePARAM수정)

--- FindYourPerfume/src/main/java/com/springriders/perfume/common/model/PerfumePARAM.java
+++ FindYourPerfume/src/main/java/com/springriders/perfume/common/model/PerfumePARAM.java
@@ -2,13 +2,22 @@ package com.springriders.perfume.common.model;
 
 public class PerfumePARAM extends PerfumeVO {
 	private int[] nt_d_c;
-
+	private int i_user;
+	
 	public int[] getNt_d_c() {
 		return nt_d_c;
 	}
 
 	public void setNt_d_c(int[] nt_d_c) {
 		this.nt_d_c = nt_d_c;
+	}
+
+	public int getI_user() {
+		return i_user;
+	}
+
+	public void setI_user(int i_user) {
+		this.i_user = i_user;
 	}
 	
 }
